6dedf2c5a19e485530ff71b93f7d7f52539125db
Unknown <gkstj0604@gmail.com>
2019-06-09 22:51:06 +0900

    Update

--- index.js
+++ index.js
@@ -34,6 +34,59 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 db_chat.run("CREATE TABLE IF NOT EXISTS chatting_room_name (name TEXT NOT NULL, message TEXT DEFAULT '', ts INT)")
+
+io.sockets.on('connection', function(socket) {
+
+  /* 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 */
+  socket.on('newUser', function() {
+
+    console.log(nick + ' 님이 접속하였습니다.')
+
+    /* 소켓에 이름 저장해두기 */
+    socket.name = nick
+
+    time = new Date()
+    entertime[socket.name]=time.getTime()
+
+    pcount++
+
+    /* 모든 소켓에게 전송 */
+    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: nick + '님이 접속하였습니다.'})
+  })
+
+  /* 전송한 메시지 받기 */
+  socket.on('message', function(data) {
+    /* 받은 데이터에 누가 보냈는지 이름을 추가 */
+    data.name = socket.name
+
+    time = new Date()
+
+    db_chat.exec("BEGIN")
+    db_chat.run("INSERT INTO chatting_room_name (name, message, ts) VALUES (?,?,?)",[data.name, data.message, time.getTime()])
+    // 자바스크립트는 기본으로 () 가 아니라 [] 를 씁니다.
+    db_chat.exec("COMMIT")
+    console.log('Name : '+data.name+', Message : '+data.message+', Time : '+time.getTime())
+
+    /* 보낸 사람을 제외한 나머지 유저에게 메시지 전송 */
+    socket.broadcast.emit('update', data)
+  })
+
+  // 채팅방 인원 확인하고 0이면 db.close()
+  socket.on('disconnect', function() {
+    console.log(socket.name + '님이 나가셨습니다.')
+
+    if(--pcount == 0)
+    {
+      db.exec("BEGIN")
+      db.run("DROP TABLE IF EXISTS chatting_room_name")
+      db.exec("COMMIT")
+    }
+
+    /* 나가는 사람을 제외한 나머지 유저에게 메시지 전송 */
+    socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
+  })
+})
+
 // http Server 시작
 server.listen(3000, function() {
     console.log('Example app listenting on port 3000!');
